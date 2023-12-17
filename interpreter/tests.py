024f598da4a709b7e2fd0d9a91263844dabdb7ed
generalsuslik <glebrusyaev77@gmail.com>
2023-12-17 15:38:54 +0300

    fuck flake with it's 79-symbol strings

--- interpreter/tests.py
+++ interpreter/tests.py
@@ -7,5 +7,6 @@ class TestInterpreter(unittest.TestCase):
     def test_interpreter1(self):
         interpreter = Interpreter("programs/program1.txt")
         interpreter.execute()
-        self.assertEqual(interpreter.coordinates, [(0, 0), (2, 0), (4, 0), (6, 0), (6, 2), (6, 4)])
+        self.assertEqual(interpreter.coordinates,
+                         [(0, 0), (2, 0), (4, 0), (6, 0), (6, 2), (6, 4)])
         self.assertEqual(interpreter.grid.get_coords(), (6, 4))
